c8dc0f9462d10281de6fd0c6c3afbbf8304aad97
samba sene <80283818+SambaSENE@users.noreply.github.com>
2023-11-17 15:06:52 +0100

    Update 05-datetime.php
     getTimeLeft() à revoir la conversion en nb mois😴😟

--- exercices-bases/exercice_1/05-datetime.php
+++ exercices-bases/exercice_1/05-datetime.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ *  retoune date en chaine de caractere
+ *
+ * @return string
+ */
 function getToday(): string
 {
     $date = date_create();
@@ -7,3 +11,36 @@ function getToday(): string
 }
 
 echo getToday();
+echo '<br>';
+
+
+/**
+ *  
+ *
+ * @param string $date
+ * @return string
+ */
+function getTimeLeft(string $date): string
+{
+    $dateActuelle = new DateTime();
+    $dateDonnee = DateTime::createFromFormat('d/m/Y', $date);
+
+    if (!$dateDonnee) {
+        return 'Format de date invalide';
+    }
+
+    $intervalle = $dateDonnee ->diff($dateActuelle) ; // (->)???  fonction pour trouver la differeence
+
+    if ($dateDonnee < $dateActuelle) {
+
+        return $intervalle ->format('%R%a jours %H heures %I minutes'); // ???
+    } elseif ($dateDonnee > $dateActuelle) {
+
+        return $intervalle ->format('%R%a jours %H heures %I minutes'); // ???
+    } else {
+        return "Aujourd'hui";
+    }
+}
+
+// Exemple d'utilisation :
+echo getTimeLeft('23/01/2024');
