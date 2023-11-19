86d66e29f4f687ced0bc4abcf14ad8185e9e0d6b
samba sene <80283818+SambaSENE@users.noreply.github.com>
2023-11-19 17:35:38 +0100

    add

--- exercices-bases/exercice_1/06-arrays.php
+++ exercices-bases/exercice_1/06-arrays.php
@@ -1,9 +1,45 @@
 <?php
+// echo phpinfo();
 
-$name = ['Joe' , 'Jack' , 'William' , 'Avrelle'];
+/**
+ *  Retourne l'element qui existe dans le tableau
+ *
+ * @param string $name
+ * @return string
+ */
+function firstItem(string $name) : string 
+{
+    $arrayNames = ['Joe' , 'Jack' , 'William' , 'Avrelle'];
+
+   if(in_array($name , $arrayNames)){
+    return $name;
+   }else {
+    return 'null';
+   }
+}
 
 
-echo $name[0];
-echo $name[1];
-echo $name[2];
-echo $name[3];
+
+echo firstItem('Joe');
+echo '<br>';
+echo firstItem('Jack');
+echo '<br>';
+echo firstItem('William');
+echo '<br>';
+echo firstItem('Avrelle');
+echo '<br>';
+echo firstItem('Macron');
+echo '<hr>';
+
+function lastItem(array $array): array
+{
+    if (!empty($array)) {
+        return end($array);
+    } else {
+        return null;
+    }
+}
+
+$arrayNames = ['Joe', 'Jack', 'William', 'Avrelle' , 'Lucky Luc'];
+echo lastItem($arrayNames);
+
