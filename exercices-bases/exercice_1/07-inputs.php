22d58c5fae5a08918738a4f3fc510fb883c81ec1
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-11-21 16:54:20 +0100

    add
    fin

--- exercices-bases/exercice_1/07-inputs.php
+++ exercices-bases/exercice_1/07-inputs.php
@@ -19,13 +19,19 @@ var_dump(stringLength('pocoyo&pato'));
 var_dump(stringLength('m_patate'));
 echo '<hr>';
 
+/**
+ * Verification mot de passe
+ *
+ * @param string $password
+ * @return boolean
+ */
 function passwordCheck(string $password): bool
 {
     // $regex = "/([^a-zA-Z0-9$])/";
 
-    if(stringLength($password)){
-        
-        if (  preg_match('/[a-z]/', $password) && preg_match('/[A-Z]/', $password) && preg_match('/[0-9]/', $password)) {
+    if (stringLength($password)) {
+
+        if (preg_match('/[a-zA-Z]/', $password) && preg_match('/[A-Z]/', $password) && preg_match('/[0-9]/', $password)) {
             return true;
         } else {
             return false;
@@ -33,4 +39,38 @@ function passwordCheck(string $password): bool
     }
 }
 
-var_dump(passwordCheck('ssenesene9'));
+var_dump(passwordCheck('senesene9'));
+echo '<hr>';
+
+
+
+/**
+ * Undocumented function
+ *
+ * @param string $name
+ * @param string $password
+ * @param array $arrayUser
+ * @return void
+ */
+function UserLogin(string $name, string $password, array $arrayUser)
+{
+    if (passwordCheck($password)) {
+
+        foreach ($arrayUser as $key => $value) {
+            if ($name === $key && $password === $value) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+$users = [
+    'joe' => 'Azer1234!',
+    'jack' => 'Azer-4321',
+    'admin' => '1234_Azer',
+];
+
+var_dump(UserLogin('joe', 'Azer1234!', $users));
+echo '<hr>';
+
---
+++ exercices-bases/exercice_1/08-html.php
@@ -0,0 +1,48 @@
+<?php
+
+            function htmlList(string $titleListe, array $arrayList)
+            {
+                if (empty($arrayList)) {
+                    echo   '<h3>' . $titleListe . '</h3>';
+                   
+                    return '<p>Aucun résulat</p>';
+                }
+
+           
+
+
+                    sort($arrayList);
+                    $name = '';
+                    foreach ($arrayList as  $value) {
+                        $name .= "<li>{$value}</li>";
+                    }
+                    
+                    $title  = '<h3>'.$titleListe.'</h3>';
+                return $title . $name;
+            }
+
+            $name = ['Stefan', 'Nathan', 'Guiaume', 'toto', 'macron'];
+            
+            ?>
+<!DOCTYPE html>
+<html lang="fr-fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <main>
+
+        <ul>
+        
+            
+                <?= htmlList('Liste des personnes', $name) ?>
+            
+        </ul>
+    </main>
+</body>
+
+</html>
